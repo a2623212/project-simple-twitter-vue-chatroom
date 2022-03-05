2a1366f63e68a60b1547210c7457f1c43ccf5698
a2623212@gmail.com <A5GjZK7w>
2022-03-05 14:26:04 +0800

    feat: add 切版chatlist

---
+++ src/components/ChatList.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="chat-list">
+    <div class="item">
+      <div class="avatar">
+        <img src="https://fakeimg.pl/300/" alt="" class="avatar" />
+      </div>
+      <div class="info"></div>
+    </div>
+  </div>
+</template>
