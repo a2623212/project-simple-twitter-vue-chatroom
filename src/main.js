e97f7065106d980c906d00f83000ef0149bc1ba9
a2623212@gmail.com <A5GjZK7w>
2022-03-05 16:13:42 +0800

    feat: install vue-socket.io

--- src/main.js
+++ src/main.js
@@ -1,13 +1,22 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
-import './styles/main.scss';
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import VueSocketIO from "vue-socket.io";
+import "./styles/main.scss";
 
 Vue.config.productionTip = false;
+
+Vue.use(
+  new VueSocketIO({
+    debug: true,
+    connection: "https://twitter-chatroom-challenge.herokuapp.com/api/chatroom",
+    vuex: {},
+  })
+);
 
 new Vue({
   router,
   store,
   render: (h) => h(App),
-}).$mount('#app');
+}).$mount("#app");
